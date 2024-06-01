b9313a1fd8fbb9acf9b18ae0d677406db9e7040c
Cautious Ndlovu <cautiendlovu94@gmail.com>
2024-06-01 15:19:56 +0200

    updated

--- components/hotel/AllHotels.tsx
+++ components/hotel/AllHotels.tsx
@@ -1,9 +1,8 @@
 "use client";
+import { useState } from "react";
 import { HotelWithRooms } from "./AddHotelForm";
-import HotelCard from "./HotelCard";
 import Image from "next/image";
-import Link from "next/link";
-import { ArrowBigRight, Dumbbell, MapPin } from "lucide-react";
+import { Dumbbell, MapPin } from "lucide-react";
 import { useRouter } from "next/navigation";
 import AmenityItem from "../AmenityItem";
 import { FaSwimmer } from "react-icons/fa";
@@ -11,11 +10,25 @@ import { Button } from "../ui/button";
 
 const AllHotels = ({ hotels }: { hotels: HotelWithRooms[] }) => {
   const router = useRouter();
+  const [currentPage, setCurrentPage] = useState(1);
+  const hotelsPerPage = 10;
+  const totalPages = Math.ceil(hotels.length / hotelsPerPage);
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
+  const startIndex = (currentPage - 1) * hotelsPerPage;
+  const selectedHotels = hotels.slice(startIndex, startIndex + hotelsPerPage);
 
   return (
     <div className="py-4 flex flex-col justify-center">
-      <div className=" grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-        {hotels.map((hotel) => (
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+        {selectedHotels.map((hotel) => (
           <div
             key={hotel.id}
             className="p-2 bg-white shadow dark:bg-gray-900"
@@ -54,6 +67,19 @@ const AllHotels = ({ hotels }: { hotels: HotelWithRooms[] }) => {
           </div>
         ))}
       </div>
+      {/* pagination goes here */}
+      {hotels.length > 10 && <div className="flex w-full mt-6 items-center justify-center gap-6">
+        <Button onClick={handlePrevPage} disabled={currentPage === 1}>
+          Prev
+        </Button>
+        <span>
+          Page {currentPage} of {totalPages}
+        </span>
+        <Button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Next
+        </Button>
+      </div>}
+      
     </div>
   );
 };
